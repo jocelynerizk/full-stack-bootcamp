14f664d24dac80cc6b300f4478722d91ae80f4ea
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-17 12:33:58 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,14 @@
+let age = 44;
+if (age < 20) {
+   console.log ("You can't enter this room.");
+  } else {
+    console.log ("You can enter this room");
+  }
+  const expression1 = 5 > 3;
+  console.log (expression1);
+  const expression2 = 10 === '10';
+  console.log (expression2);
+  const expression3 = true && false;
+  console.log (expression3);
+  const expression4 = !expression3;
+  console.log (expression4);
