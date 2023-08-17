d6acf7552f1387dc9dab79be943f545e48bec45e
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-17 10:46:51 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -7,3 +7,21 @@ alert ("my name  is "+name);
 console.log(lastname);
 console.log(age);
 console.log(dob);
+
+let str = "This is a text";
+console.log(str);
+const flag1 = new Boolean(true);
+console.log(flag1.toString());
+const flag3 = 1215;
+console.log(flag3);
+const flag4 = 999;
+var str1 = flag4.toString()
+console.log(str1);
+var str2 = "{}";
+console.log(str2);
+var str3 = "[]";
+console.log(str3);
+const str4 ="null";
+console.log(str4)
+const str5="undefined";
+console.log(str5);
