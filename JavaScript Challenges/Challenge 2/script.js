abaf4273d1e42b948d89b4c45209e865ea302f9b
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-17 13:05:08 +0300

    Challenge 2 - Done with step 2

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -12,3 +12,25 @@ if (age < 20) {
   console.log (expression3);
   const expression4 = !expression3;
   console.log (expression4);
+  const age1 = 10;
+const hasDriverLicense = false;
+const hasCar = false;
+const canDrive = false;
+if (age1 >= 18 && hasDriverLicense == true) {
+    let canDrive = true;
+    console.log (canDrive);
+    console.log("can drive")
+   }
+   
+let canRentCar = false;
+if (age1 >= 22 && hasDriverLicense == true && hasCar == false) {
+    let canRentCar = true;
+    console.log (canRentCar);
+    console.log("can rent")
+   }
+   let needDriver = false;
+
+   if (age1 <18 && hasDriverLicense == false && hasCar == false) {
+    let needDriver = true;
+    console.log("need driver")
+   }
