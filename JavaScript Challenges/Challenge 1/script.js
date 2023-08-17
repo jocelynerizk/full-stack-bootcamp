425d981942b2e5786ea4977f1b4ade741d56e335
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-17 10:15:09 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,6 +1,9 @@
 console.log('I am linked now!');
 var name = "Jocelyne";
-var lastname = "Rizk"
-let age = 44
-const d = new Date("1979-03-1cd5"
+var lastname = "Rizk";
+let age = 44;
+const dob = new Date("1979-03-15");
 alert ("my name  is "+name);
+console.log(lastname);
+console.log(age);
+console.log(dob);
