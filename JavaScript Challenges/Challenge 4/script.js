be7b0bde0a0e13ba888120f1329d5da5470dea78
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-22 21:08:50 +0300

    Challenge 4 - Done with step 4

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -28,3 +28,34 @@ console.log(thirdNum);
 var { Name , LOCATION , NATIONALITY } = personalInfo;
 
 console.log( Name , LOCATION , NATIONALITY);
+const teacher = {
+name: "Brown",
+address: {
+area: {
+street: "oxford university road",
+},
+city: "London",
+country: "England"
+},
+}
+console.log(teacher.address.area);
+console.log(teacher.address);
+
+
+const [firstNumber, secondNumber, ...restOfNumbers] = fiveNumbers;
+
+console.log("First Number:", firstNumber);    // Output: First Number: 1
+console.log("Second Number:", secondNumber);  // Output: Second Number: 2
+console.log("Rest of Numbers:", restOfNumbers); // Output: Rest of Numbers: [3, 4, 5]
+
+// Two objects to be merged
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+
+// Using the spread operator
+const mergedObj1 = { ...obj1, ...obj2 };
+console.log(mergedObj1); // Output: { a: 1, b: 3, c: 4 }
+
+// Using Object.assign()
+const mergedObj2 = Object.assign({}, obj1, obj2);
+console.log(mergedObj2); // Output: { a: 1, b: 3, c: 4 }
