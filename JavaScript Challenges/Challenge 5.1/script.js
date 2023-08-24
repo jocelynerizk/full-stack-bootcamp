ff53222ebe3a340ae83e86fe72822de4d3bb214d
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-08-24 15:17:45 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -1,8 +1,9 @@
+let x= document.querySelector("body")
 var button = document.createElement("button");
-button.innerHTML = "Click Me"; 
+button.textContent = "Click Me";
+x.appendChild(button);
 
 
-document.body.appendChild(button);
 button.style.padding = "10px 20px";
 button.style.fontSize = "16px";
 button.style.backgroundColor = "#3498db";
@@ -14,8 +15,35 @@ button.addEventListener("click", function() {
     button.style.backgroundColor = "red";
     button.style.color = "white"
   });
-  
+
   button.addEventListener("click", function() {
     var randomNumber = Math.floor(Math.random() * 1000) + 1;
     button.innerHTML = "Clicked " + randomNumber + "!";
+  });
+
+
+
+  var heading = document.createElement("h1");
+  heading.textContent = "Hello, World!"; 
+  x.appendChild(heading);
+
+  heading.style.border = "10px dotted green";
+
+ document.addEventListener("keydown", function(event) {
+ var key = event.key.toLowerCase();
+
+    if (key === 'a') {
+     heading.style.backgroundColor = "red";
+    } else if (key === 's') {
+     heading.style.marginLeft = parseInt(heading.style.marginLeft || 0) + 10 + "px";
+    } else if (key === 'd') {
+      var paragraph = document.createElement("p");
+      paragraph.textContent = "Key D was pressed!";
+      x.appendChild(paragraph);
+    } else if (key === 'w') {
+        heading.style.display = heading.style.display === "none" ? "block" : "none";
+    } else if (key === ' ') {
+      var fontSize = parseInt(window.getComputedStyle(heading).fontSize);
+      heading.style.fontSize = (fontSize + 4) + "px";
+    }
   });
